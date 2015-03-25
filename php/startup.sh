5513d6b27a9c850d9859e3258f8e6510319a5d82
Paolo Galeone <nessuno@nerdz.eu>
2015-03-25 05:12:24 -0400

    Use of better namings

--- php/startup.sh
+++ php/startup.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
-
-export GOPATH=/srv/http/go
-export PATH=$PATH:$GOPATH/bin
-
 if [ ! -d /srv/http/nerdz.eu ]; then
-    mkdir -p /srv/http/go
     cd /srv/http
     git clone --recursive https://github.com/nerdzeu/nerdz.eu.git
-    git clone https://github.com/nerdzeu/nerdz-test-db.git
-    go get -u github.com/nerdzeu/nerdz-api
-
     cd nerdz.eu
     composer install
 fi
