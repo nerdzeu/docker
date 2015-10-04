56f362cd2a1c61374330833f1c28e8ea21ebb28b
alfateam123 <alfateam123@gmail.com>
2015-10-04 12:18:30 -0400

    [WIP] camo starts, but it can't be reached

--- autosetup.sh
+++ autosetup.sh
@@ -12,9 +12,9 @@ echo "putting your hostname ($1) into nginx-reverse-proxy"
 cat nginx-reverse-proxy | sed "s/yourhost/$1/g" > nginx-reverse-proxy.custom
 
 #automatically copy nginx-reverse-proxy.custom and certs to the right folders
-#echo "next steps will require sudo as nginx folder is owned by root (somehow)"
-#echo "copying your reverse proxy conf to /etc/nginx/sites-enabled/$1"
-#sudo cp nginx-reverse-proxy.custom /etc/nginx/sites-enabled/$1
-#echo "putting nerdz certs into nginx folders..."
-#sudo mkdir -p /etc/nginx/ssl
-#sudo cp nginx/conf/certs/nerdz.* /etc/nginx/ssl
+echo "next steps will require sudo as nginx folder is owned by root (somehow)"
+echo "copying your reverse proxy conf to /etc/nginx/sites-enabled/$1"
+sudo cp nginx-reverse-proxy.custom /etc/nginx/sites-enabled/$1
+echo "putting nerdz certs into nginx folders..."
+sudo mkdir -p /etc/nginx/ssl
+sudo cp nginx/conf/certs/nerdz.* /etc/nginx/ssl
