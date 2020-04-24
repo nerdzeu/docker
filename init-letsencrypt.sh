2c8d7e65a5c4835422c111bf06b744a27d27ac27
Paolo Galeone <nessuno@nerdz.eu>
2020-04-24 21:23:13 +0200

    init-letsencrypt: single certificate for multiple domain/sub

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -40,15 +40,14 @@ fi
 
 echo "### Creating dummy certificate for $domains ..."
 
-for domain in "${domains[@]}"; do
-    path="/etc/letsencrypt/live/$domain"
-    mkdir -p "$data_path/conf/live/$domain"
-    docker-compose run --rm --entrypoint "\
-      openssl req -x509 -nodes -newkey rsa:1024 -days 1\
-        -keyout '$path/privkey.pem' \
-        -out '$path/fullchain.pem' \
-        -subj '/CN=localhost'" certbot
-done
+path="/etc/letsencrypt/live/$domains"
+mkdir -p "$data_path/conf/live/$domains"
+docker-compose run --rm --entrypoint "\
+    openssl req -x509 -nodes -newkey rsa:1024 -days 1\
+    -keyout '$path/privkey.pem' \
+    -out '$path/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
+
 
 echo "### Starting nginx ..."
 docker-compose up --force-recreate -d nginx
@@ -56,13 +55,10 @@ echo
 
 echo "### Deleting dummy certificate for $domains ..."
 
-for domain in "${domains[@]}"; do
-    docker-compose run --rm --entrypoint "\
-      rm -Rf /etc/letsencrypt/live/$domain && \
-      rm -Rf /etc/letsencrypt/archive/$domain && \
-      rm -Rf /etc/letsencrypt/renewal/$domain.conf" certbot
-    echo "$domain"
-done
+docker-compose run --rm --entrypoint "\
+    rm -Rf /etc/letsencrypt/live/$domains && \
+    rm -Rf /etc/letsencrypt/archive/$domains && \
+    rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
 #Join $domains to -d args
@@ -81,7 +77,7 @@ esac
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker-compose run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
+    certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
     $domain_args \
