f2b44d5644631dbe5f62ee9fc0be578d25e55a2d
Paolo Galeone <nessuno@nerdz.eu>
2022-07-03 14:47:53 +0200

    update support

--- postgres/init/init_postgres.sh
+++ postgres/init/init_postgres.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 schema=$(dirname $BASH_SOURCE[0])/create/schema.pgsql
-dump=$(dirname $BASH_SOURCE[0])/migrate/v1_dump.pgsql
-if [ ! -f "$schema" ] && [ ! -f "$dump" ]; then
+v1dump=$(dirname $BASH_SOURCE[0])/migrate/v1_dump.pgsql
+upgrade=$(dirname $BASH_SOURCE[0])/upgrade.pgsql
+
+if [ ! -f "$schema" ] && [ ! -f "$v1dump" ] && [ ! -f "$upgrade" ]; then
     echo "Missing create/schema.pgsql and migrate/v1_dump.pgsql"
     echo "At least once is required."
     exit -1
@@ -17,16 +19,21 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     ALTER DATABASE nerdz OWNER to nerdz;
 EOSQL
 
+
+if [ -f "$upgrade" ]; then
+    psql -v --username "$POSTGRES_USER" --dbname "nerdz" < "$upgrade"
+    exit 0
+fi
 # If we have the v1_dump, we restore it and apply the migration script to convert it
 # to nerdz v2
-if [ -f "$dump" ]; then
+if [ -f "$v1dump" ]; then
     # Restore v1
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "nerdz" < "$dump"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "nerdz" < "$v1dump"
 
     # Convert to v2
     db_patch=$(dirname $BASH_SOURCE[0])/migrate/patch.pgsql
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "nerdz" < "$db_patch"
-else
+elif [ -f "$v1dump" ]; then
     # Initialize the db, use superuser because we need to create extensions
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "nerdz" < "$schema"
 fi
